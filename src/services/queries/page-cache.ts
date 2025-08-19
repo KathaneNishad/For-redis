389e899c783f026df147484eef303df6dd1fb6b8
LambSauce_NK <nikkong16@gmail.com>
2025-08-20 01:25:24 +0530

    Add a method to generate Key to avoid typo mistakes

--- src/services/queries/page-cache.ts
+++ src/services/queries/page-cache.ts
@@ -1,10 +1,12 @@
 import { client } from "$services/redis";
+import { pageCacheKey } from "$services/keys";
+
 const cacheRoutes= ['/about','/privacy','/auth/signup'];
 
 export const getCachedPage = (route: string) => {
     if(cacheRoutes.includes(route)){
         //GET key
-        return client.get('pagecache#' + route);  //pagecahce#route : this will be out key 
+        return client.get(pageCacheKey(route));  //pagecahce#route : this will be out key 
     }
     return null;
 };
@@ -12,7 +14,7 @@ export const getCachedPage = (route: string) => {
 export const setCachedPage = (route: string, page: string) => {
     if(cacheRoutes.includes){
         //SET key value EX 5 
-        return client.set('pagecache#' + route, page,{
+        return client.set(pageCacheKey(route), page,{
             EX: 5  // Expiration time
         })
     }
