e34b8c506a2f76750b661829c18a4aa166310a75
LambSauce_NK <nikkong16@gmail.com>
2025-08-20 01:19:29 +0530

    Added Page Caching using redis-client

--- src/services/queries/page-cache.ts
+++ src/services/queries/page-cache.ts
@@ -1,3 +1,19 @@
-export const getCachedPage = (route: string) => {};
+import { client } from "$services/redis";
+const cacheRoutes= ['/about','/privacy','/auth/signup'];
 
-export const setCachedPage = (route: string, page: string) => {};
+export const getCachedPage = (route: string) => {
+    if(cacheRoutes.includes(route)){
+        //GET key
+        return client.get('pagecache#' + route);  //pagecahce#route : this will be out key 
+    }
+    return null;
+};
+
+export const setCachedPage = (route: string, page: string) => {
+    if(cacheRoutes.includes){
+        //SET key value EX 5 
+        return client.set('pagecache#' + route, page,{
+            EX: 5  // Expiration time
+        })
+    }
+};
