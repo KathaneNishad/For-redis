66850e496a6e5182a9d496ce06a48ab76ce2e7e8
LambSauce_NK <nikkong16@gmail.com>
2025-08-21 02:45:43 +0530

    Add serialize and deserialize method for getUserByID

--- sandbox/index.ts
+++ sandbox/index.ts
@@ -1,5 +1,14 @@
 import 'dotenv/config';
 import { client } from '../src/services/redis';
 
-const run = async () => {};
+const run = async () => {
+    await client.hSet('company',{
+        name : 'CT',
+        domain : 'HC',
+        year : '2255'
+    })
+
+    const comp = await client.hGetAll('company');
+    console.log(comp);
+};
 run();
--- src/services/queries/users.ts
+++ src/services/queries/users.ts
@@ -1,8 +1,39 @@
+import { client } from '$services/redis';
 import type { CreateUserAttrs } from '$services/types';
 import { genId } from '$services/utils';
+import { usersKey } from '$services/keys';  // 
 
 export const getUserByUsername = async (username: string) => {};
 
-export const getUserById = async (id: string) => {};
+export const getUserById = async (id: string) => {
+    const user = await client.hGetAll(usersKey(id));
+    return deserialize(id,user);
+};
 
-export const createUser = async (attrs: CreateUserAttrs) => {};
+
+export const createUser = async (attrs: CreateUserAttrs) => {
+    const id = genId();
+    await client.hSet(usersKey(id),serialize(attrs));
+    return id
+};
+
+//Serialize function => what ever object/data we got is convert to redis format 
+//simply putting it everything will be type of string 
+//we are not mentioning id in our serialized obj
+const serialize =(user:CreateUserAttrs) =>{
+    return {
+        username : user.username,
+        password : user.password
+    }
+};
+
+//deserealize function for getUserById
+//while deseializing we mention id (this is optional as per use)
+//deserialize will return object value with their orignal types
+const deserialize =(id:string,user:{[key:string]:string})=>{
+    return {
+        id,
+        username : user.username,
+        password : user.password
+    }
+};
