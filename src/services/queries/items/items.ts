ae128d20682b399b49e967c3b30557ba11e96cba
LambSauce_NK <nikkong16@gmail.com>
2025-08-23 03:04:08 +0530

    getItems impl usinf Promise

--- src/services/queries/items/items.ts
+++ src/services/queries/items/items.ts
@@ -16,7 +16,20 @@ export const getItem = async (id: string) => {
     return deserialize(id,item);
 };
 
-export const getItems = async (ids: string[]) => {};
+export const getItems = async (ids: string[]) => { //ids : arrays of item ids
+    const command = ids.map((id)=>{
+        return client.hGetAll(itemsKey(id)); // here it will run the command for each id from ids[] and call async redis call(hGetAll) then store them in a array
+    })
+
+    const result = await Promise.all(command); // Promise.all() will take each command result in parallel and store them in array of object
+
+    return result.map((result,i)=>{
+        if(Object.keys(result).length === 0){
+        return null;
+    }
+    return deserialize(ids[i],result);
+    })
+};
 
 export const createItem = async (attrs: CreateItemAttrs) => {
     //generates an ID
