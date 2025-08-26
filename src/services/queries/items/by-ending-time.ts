e807bb1285994e2eea02463e188ba7372a4179e8
LambSauce_NK <nikkong16@gmail.com>
2025-08-27 02:39:20 +0530

    IMPL for items expiring at soonest time :: first part

--- src/services/queries/items/by-ending-time.ts
+++ src/services/queries/items/by-ending-time.ts
@@ -1,5 +1,23 @@
+import { itemsByEndingAtKey } from "$services/keys";
+import { client } from "$services/redis";
+
 export const itemsByEndingTime = async (
 	order: 'DESC' | 'ASC' = 'DESC',
-	offset = 0,
-	count = 10
-) => {};
+	offset = 0, //values we want to skip
+	count = 10 //values we want 
+) => {
+	//get the item ids have endingAt time that are soonest or time between now and future
+	const ids = await client.zRange(itemsByEndingAtKey(),  //set
+	Date.now(), // starting value
+	'+inf', //ending value for range ,can say future
+	{
+		BY: 'SCORE', //SORT by score,
+		LIMIT: {     // Functions for ssorted set
+			offset,
+			count
+		}
+
+	}
+);
+console.log(ids); // load items ids that are soonest
+};
